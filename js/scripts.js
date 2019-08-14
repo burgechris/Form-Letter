269881a17dba0af80667190bb2da84c871735d12
Christopher Burge and Neha Malhotra <christopher@>
2019-08-14 10:23:56 -0700

    adding Jquery in our project

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,28 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var firstNameInput = $("input#firstName").val();
-        var lastNameInput = $("input#lastName").val();
-        var streetInput= $("input#street").val();
-        var cityInput = $("input#city").val();
-        var stateInput = $("input#state").val();
-        var zipInput = $("input#zip").val();
+    var lastNameInput = $("input#lastName").val();
+    var streetInput = $("input#street").val();
+    var cityInput = $("input#city").val();
+    var stateInput = $("input#state").val();
+    var zipInput = $("input#zip").val();
 
 
+    $(".firstName").text(firstNameInput);
+    $(".lastName").text(lastNameInput);
+    $(".street").text(streetInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zip").text(zipInput);
 
-     $(".firstName").text(firstNameInput);
-     $(".lastName").text(lastNameInput);
-     $(".street").text(streetInput);
-     $(".city").text(cityInput);
-     $(".state").text(stateInput);
-     $(".zip").text(zipInput);
+    $("#info").show();
+    event.preventDefault();
+  });
+});
 
-     $("#info").show();
-     event.preventDefault();
-   });
-
-
-
-
+$(document).ready(function() {
+  $("#story").click(function() {
+    $("#bob").show();
+    $("#image").show();
+  });
 });
