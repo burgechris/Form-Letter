9deaab165a2169339676dbbb5299470ab89716c3
Christopher Burge and Neha Malhotra <christopher@>
2019-08-14 09:39:38 -0700

    add html, css, js files

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var firstNameInput = $("input#firstName").val();
+        var lastNameInput = $("input#lastName").val();
+        var streetInput= $("input#street").val();
+        var cityInput = $("input#city").val();
+        var stateInput = $("input#state").val();
+        var zipInput = $("input#zip").val();
+
+
+
+     $(".firstName").text(firstNameInput);
+     $(".lastName").text(lastNameInput);
+     $(".street").text(streetInput);
+     $(".city").text(cityInput);
+     $(".state").text(stateInput);
+     $(".zip").text(zipInput);
+
+     $("#info").show();
+     event.preventDefault();
+   });
+
+
+
+
+});
